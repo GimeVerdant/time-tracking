4514650430e3f5a4eb2d5a576c69b9db49dd5c98
Gimena <g.blanco@asys.nl>
2026-03-15 11:37:17 +0100

    js daily, monthly, weekly

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-// ===========================
-// GET DOM ELEMENTS
-// ===========================
-const form = document.getElementById('form');
+fetch('./data.json')
+  .then(response => response.json())
+  .then(data => {
+
+    function updateCards(timeframe) {
+      data.forEach(activity => {
+        const cardClass = activity.title.toLowerCase().replaceAll(' ', '-')
+        const card = document.querySelector(`.report__card--${cardClass}`)
+        card.querySelector('.report__current').textContent = `${activity.timeframes[timeframe].current}hrs`
+        card.querySelector('.report__previous').textContent = `Previous - ${activity.timeframes[timeframe].previous}hrs`
+      })
+    }
+
+    updateCards('weekly')
+
+    document.querySelectorAll('.profile__item').forEach(item => {
+      item.addEventListener('click', () => {
+        const timeframe = item.textContent.toLowerCase()
+        document.querySelectorAll('.profile__item').forEach(i => i.classList.remove('profile__item--active'))
+        item.classList.add('profile__item--active')
+        updateCards(timeframe)
+      })
+    })
+
+  })
