c8f77ab1ab47bf6c9a4c9bd0fcbb50d2e5548430
Gimena <g.blanco@asys.nl>
2026-03-15 14:14:26 +0100

    added Readme and comments

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// Fetch data
 fetch('./data.json')
   .then(response => response.json())
   .then(data => {
+
+    // Loop through each activity and update its card with the current and previous hours
 
     function updateCards(timeframe) {
       data.forEach(activity => {
@@ -11,13 +14,21 @@ fetch('./data.json')
       })
     }
 
+    // Set data to weekly on load
     updateCards('weekly')
 
+    // Update data on click
     document.querySelectorAll('.profile__item').forEach(item => {
       item.addEventListener('click', () => {
         const timeframe = item.textContent.toLowerCase()
-        document.querySelectorAll('.profile__item').forEach(i => i.classList.remove('profile__item--active'))
+        document.querySelectorAll('.profile__item').forEach(i => {
+          i.classList.remove('profile__item--active')
+          i.setAttribute('aria-selected', 'false')
+          i.setAttribute('tabindex', '-1')
+        })
         item.classList.add('profile__item--active')
+        item.setAttribute('aria-selected', 'true')
+        item.setAttribute('tabindex', '0')
         updateCards(timeframe)
       })
     })
